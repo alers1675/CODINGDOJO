4e7bdac548d81820acd4216eb8ded1bb02fae752
alers1675 <alexander.meneses.urra@alumnos.sip.cl>
2022-04-07 12:03:55 -0400

    Tercer cambio

--- Meneses_Alexander_JsPrediccion.js
+++ Meneses_Alexander_JsPrediccion.js
@@ -8,14 +8,14 @@ function myBirthYearFunc(){
 //Predicción 2: si necesitáramos enviar un año de nacimiento hacia la función, le diríamos a la función "oye, te enviaremos una variable llamada EntradaAñoNacimiento". Lo hacemos agregando el nombre de la variable entre paréntesis. Mira a continuación un ejemplo. 
 
 //Entonces, si la variable EntradaAñoNacimiento es 1980 (también conocida como var EntradaAñoNacimiento = 1980), y se llama a esta función, ¿qué indicará console.log?
-
+//Lo que indicara el console,log es que la EntradaAñoNacimiento es = 1980
 function myBirthYearFunc(EntradaAñoNacimiento){
     console.log("Nací en " + EntradaAñoNacimiento);
 }
 
 
 //Predicción 3: Intentémoslo otra vez. Si var num1 = 10 y var num2 = 20, ¿qué indicaría console.log?
-//
+//Lo que mostrara el console.log es la suma del var num1 + var num2 que es 30
 function add(num1, num2){
     console.log("¡Sumando números!");
     console.log("num1 is: " + num1);
